dffee9a474dc9dc6453d7a1556b984a1b55b9c44
Jake Woods <jake@jakewoods.net>
2018-03-01 21:58:48 +1100

    Add the beginnings of levels

--- src/Simulate.hs
+++ src/Simulate.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE DataKinds     #-}
+
 module Simulate (initializeWorld, step) where
 
 import Data.Ecstasy
+import Data.Foldable
 import Linear.V2
-import Control.Monad (void)
+import Control.Monad
 import qualified Graphics.Gloss.Data.Color as G
 
 import Types
@@ -10,30 +13,44 @@ import Simulate.Physics
 
 initializeWorld :: GameSystem ()
 initializeWorld = do
-  void $ newEntity $ defEntity
-    { position = Just (V2 50 100)
-    , geometry = Just $ Box 100 25
-    , Types.color = Just G.red
-    , followMouseX = Just ()
-    , frozen = Just ()
-    }
-
-  void $ newEntity $ defEntity
-    { position = Just (V2 0 0)
-    , velocity = Just (V2 40 40)
-    , geometry = Just $ Box 100 25
-    , Types.color = Just G.green
-    , bouncy = Just ()
-    , frozen = Just ()
-    }
-
-  void $ newEntity $ defEntity
-    { position = Just (V2 10 10)
-    , velocity = Just (V2 (-50) (30))
-    , geometry = Just $ Box 100 25
-    , Types.color = Just G.blue
-    , bouncy = Just ()
-    }
+  ball
+  paddle
+  blockLine (V2 (-290) 310) (V2 290 310) 11
 
 step :: Float -> GameSystem ()
 step delta = stepPhysics delta
+
+-- TODO: An actual level system.
+paddle :: GameSystem ()
+paddle = void $ newEntity $ defEntity
+  { position = Just (V2 0 (-220))
+  , geometry = Just $ Box 100 10
+  , Types.color = Just G.green
+
+  , frozen = Just ()
+  , followMouseX = Just()
+  }
+
+ball :: GameSystem ()
+ball = void $ newEntity $ defEntity
+  { position = Just (V2 (-50) (-180))
+  , velocity = Just (V2 50 (-50))
+  , geometry = Just $ Box 10 10
+  , Types.color = Just G.red
+
+  , frozen = Just ()
+  , bouncy = Just()
+  }
+
+block :: V2 Float -> GameSystem ()
+block pos = void $ newEntity $ defEntity
+  { position = Just pos
+  , geometry = Just $ Box 50 10
+  , Types.color = Just G.blue
+  }
+
+blockLine :: V2 Float -> V2 Float -> Int -> GameSystem ()
+blockLine start end blocks = traverse_ block positions
+  where
+    scale = (abs $ end - start) / (fromIntegral blocks)
+    positions = fmap (\i -> start + scale * fromIntegral i) [1..blocks]
