280d953ebad05d335378a78283f5782fd701f3ae
Jake Woods <jake@jakewoods.net>
2018-03-22 17:32:17 +1100

    Enable profiling

--- shell.nix
+++ shell.nix
@@ -7,6 +7,12 @@ let
 
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = haskellNew: haskellOld: rec {
+      # We want to enable profiling for all our dependencies in nix-shell
+      # so we can profile stuff!
+      mkDerivation = args: haskellOld.mkDerivation (args // {
+        enableLibraryProfiling = true;
+      });
+
       # OpenGLRaw segfaults when we try to build it's haddocks
       OpenGLRaw = pkgs.haskell.lib.dontHaddock haskellOld.OpenGLRaw;
 
@@ -14,6 +20,8 @@ let
       linear = pkgs.haskell.lib.dontCheck haskellOld.linear;
     };
   };
+
+        unstable.haskellPackages.ghc-prof-flamegraph
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
